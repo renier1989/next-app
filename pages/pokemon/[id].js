172fd82f89811267f4edbec1b695dfd45874370f
Renier Vargas <renier1989@gmail.com>
2023-08-25 14:25:58 -0500

    Class172 - Agregando SSR a una pagina

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,10 +1,17 @@
-import { useRouter } from "next/router"
 
-function PokemonDetail() {
-    const router = useRouter();
+function PokemonDetail({data}) {
+    console.log(data)
   return (
-    <div>PokemonDetail {router.query.id}</div>
+    <div>PokemonDetail </div>
   )
+}
+
+// esta funcion se usa para el SSR (Server Side Rendering)
+export const getServerSideProps = async ({params}) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
+    const data = await response.json();
+
+    return {props : {data}}
 }
 
 export default PokemonDetail
